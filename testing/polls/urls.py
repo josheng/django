1ad4ea7b9eedd01763a3fa7250c615f4c17292b8
Joshua Eng <engch28@gmail.com>
2021-12-25 00:27:57 +0800

    set up route and views

--- testing/polls/urls.py
+++ testing/polls/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'), # assuming this is ref to views file with index method, first arg is route
+    path('test', views.test, name='test'), # have to define a route name
 ]
--- testing/polls/views.py
+++ testing/polls/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
 # Create your views here.
+
+def test(request):
+    return HttpResponse("<h1>Hello, world. You're at the polls test.</h1>") #looks like can take in html tags

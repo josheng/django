349858e30c0434a6c33f0434df1b3318889fb5f4
Joshua Eng <engch28@gmail.com>
2021-12-25 00:55:28 +0800

    set up postgres

--- testing/polls/urls.py
+++ testing/polls/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'), # assuming this is ref to views file with index method, first arg is route
-    path('test', views.test, name='test'), # have to define a route name
+    path('test', views.test, name='test'), # have to define a route name, name arg is probably like rails route name
 ]
--- testing/testing/settings.py
+++ testing/testing/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'testing.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangopolls',
+        'USER': 'ech',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -107,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Singapore'
 
 USE_I18N = True
 
